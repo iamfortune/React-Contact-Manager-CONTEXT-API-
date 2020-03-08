2e3794216c82d8c74cd0066c51fa3e599fbd2307
iamfortune <ikechifortune@gmail.com>
2020-03-08 11:13:18 +0100

    added contact information

---
+++ src/components/Contact.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Contact extends Component {
+    render() {
+        return (
+            <div>
+                <h4>Francy Ana</h4>
+                <li>francylove@gmail.com</li>
+                <li>Phone: 08109542715</li>
+            </div>
+        );
+    }
+}
+
+export default Contact;
