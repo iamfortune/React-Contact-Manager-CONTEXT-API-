52fc0c831c33f6615536ed0cdc78655f53a9eef4
iamfortune <ikechifortune@gmail.com>
2020-03-08 14:54:10 +0100

    added new files on App component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
+import Contact from './components/Contact';
 import './App.css';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>The App Component</h1>
-      
+        <Contact />
       </div>
     );
   }
