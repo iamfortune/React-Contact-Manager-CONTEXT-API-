699746971d05c7861ec5e26dc73068f6e8a2f49b
iamfortune <ikechifortune@gmail.com>
2020-03-08 11:00:28 +0100

    modified files for app component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,10 @@ import './App.css';
 
 class App extends Component {
   render() {
-    const name = 'Fortune';
     return (
       <div className="App">
         <h1>The App Component</h1>
-        <h4>Hello {name.toUpperCase()} </h4>
+      
       </div>
     );
   }
