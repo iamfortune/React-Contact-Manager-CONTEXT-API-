b2550b0b06c8fd4b7851fbb450464d51d745e28a
iamfortune <ikechifortune@gmail.com>
2020-03-10 16:42:36 +0100

    added default props

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header branding="Kay's Contact Manager" />
+        <Header />
         <Contact
           name="Francy Ana"
           email="francylove@gmail.com"
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,23 @@
-import React from 'react'
+import React from 'react';
+import PropTypes from 'prop-types';
+
 
 const Header = (props) => {
     const { branding } = props;
     return(
         <div>
-            <h1>{props.branding}</h1>
+            <h1>{branding}</h1>
         </div>
     );
 }
+
+
+Header.defaultProps = {
+    branding: "My App"
+};
+
+Header.propTypes = {
+    branding: PropTypes.string.isRequired
+};
 
 export default Header;
