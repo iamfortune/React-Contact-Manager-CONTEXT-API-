dbd88ed4848fbec0bf8c86487d60de8233050928
iamfortune <ikechifortune@gmail.com>
2020-03-12 07:09:16 +0100

    added navbar using bootstrap to the header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,9 +5,12 @@ import PropTypes from 'prop-types';
 const Header = (props) => {
     const { branding } = props;
     return(
-        <div>
-            <h1>{branding}</h1>
-        </div>
+        <nav className="navbar navbar-expand-sm navbar-dark bg-danger mb-3 py-0">
+            <div className="container">
+                <a href="/" className="navbar-brand">
+                    {branding}</a>
+            </div>
+        </nav>
     );
 }
 
