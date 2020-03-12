2efe0cf1853d06e5d746b6789ee3bdc470e241e1
iamfortune <ikechifortune@gmail.com>
2020-03-12 05:55:43 +0100

    deleted stylesheets in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ const Header = (props) => {
     const { branding } = props;
     return(
         <div>
-            <h1 style={{color: 'red', fontSize: '50px'}}>{branding}</h1>
+            <h1>{branding}</h1>
         </div>
     );
 }
@@ -20,9 +20,5 @@ Header.propTypes = {
     branding: PropTypes.string.isRequired
 };
 
-const headingStyle = {
-    color: 'red',
-    fontSize: '50px'
-};
 
 export default Header;
