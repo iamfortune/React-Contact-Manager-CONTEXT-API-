78f04c0a3fb719e880c3a22f946bd1f7ee2e87a3
iamfortune <ikechifortune@gmail.com>
2020-03-12 07:28:05 +0100

    used bootstrap classes to make for a better contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,10 +6,12 @@ class Contact extends Component {
     render() {
         const { name, email, phone } = this.props;
         return (
-            <div>
+            <div className="card card-body mb-3">
                 <h4>{name}</h4>
-                <li>Email: {email}</li>
-                <li>Phone: {phone}</li>
+                <ul className="list-group">
+                <li className="list-group-item">Email: {email}</li>
+                <li className="list-group-item">Phone: {phone}</li>
+                </ul>
             </div>
         );
     }
