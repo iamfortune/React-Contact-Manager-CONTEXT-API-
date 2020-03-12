7a53b233b2e3d495b8011bbd09cd866e1d879671
iamfortune <ikechifortune@gmail.com>
2020-03-12 07:08:36 +0100

    modified files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Contact from './components/Contact';
 import Header from "./components/Header";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 
