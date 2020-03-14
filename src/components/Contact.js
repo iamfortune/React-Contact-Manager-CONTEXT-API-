7338846d33eb1fc528588b9f4ff8572364b63b27
iamfortune <ikechifortune@gmail.com>
2020-03-15 00:10:09 +0100

    added the onClick function to icon

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,21 +7,15 @@ class Contact extends Component {
         showContactInfo: true
     };
 
-
-    onShowClick = e => {
-        this.setState({ showContactInfo: 
-            !this.state.showContactInfo
-         });
-    };
-
-
+  
     render() {
         const { name, email, phone } = this.props.contact;
         return (
             <div className="card card-body mb-3">
                 <h4>
                     {name} <i 
-                    onClick={this.onShowClick}
+                    onClick={() => this.setState({
+                        showContactInfo: !this.state.showContactInfo })}
                     className="fas fa-sort-down" />
                 </h4>
                 <ul className="list-group">
