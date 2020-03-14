1006ef29667613d0302445005f91667c69c29939
iamfortune <ikechifortune@gmail.com>
2020-03-14 16:02:45 +0100

    destructured propTypes to accept contact as the only prop

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,13 +4,13 @@ import PropTypes from 'prop-types';
 
 class Contact extends Component {
     render() {
-        const { contact } = this.props;
+        const { contact } = this.props.contact;
         return (
             <div className="card card-body mb-3">
-                <h4>{contact.name}</h4>
+                <h4>{name}</h4>
                 <ul className="list-group">
-                <li className="list-group-item">Email: {contact.email}</li>
-                <li className="list-group-item">Phone: {contact.phone}</li>
+                <li className="list-group-item">Email: {email}</li>
+                <li className="list-group-item">Phone: {phone}</li>
                 </ul>
             </div>
         );
@@ -18,9 +18,7 @@ class Contact extends Component {
 }
 
 Contact.propsTypes = {
-    name: PropTypes.string.isRequired,
-    email: PropTypes.string.isRequired,
-    phone: PropTypes.string.isRequired
-}
+    contact: PropTypes.object.isRequired
+};
 
 export default Contact;
