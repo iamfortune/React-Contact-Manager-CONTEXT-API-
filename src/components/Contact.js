d21a9dfb6630cf0a8d91cc4f533f75db3ba6b763
iamfortune <ikechifortune@gmail.com>
2020-03-14 15:58:44 +0100

    destructured and added contact as props

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,13 +4,13 @@ import PropTypes from 'prop-types';
 
 class Contact extends Component {
     render() {
-        const { name, email, phone } = this.props;
+        const { contact } = this.props;
         return (
             <div className="card card-body mb-3">
-                <h4>{name}</h4>
+                <h4>{contact.name}</h4>
                 <ul className="list-group">
-                <li className="list-group-item">Email: {email}</li>
-                <li className="list-group-item">Phone: {phone}</li>
+                <li className="list-group-item">Email: {contact.email}</li>
+                <li className="list-group-item">Phone: {contact.phone}</li>
                 </ul>
             </div>
         );
