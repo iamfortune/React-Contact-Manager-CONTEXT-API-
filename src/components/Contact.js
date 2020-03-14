828d3b27c7dda2e01899224391e86a1bc9ca11cc
iamfortune <ikechifortune@gmail.com>
2020-03-14 17:02:11 +0100

    added new click event function on the icon

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,12 +3,19 @@ import PropTypes from 'prop-types';
 
 
 class Contact extends Component {
+    onShowClick() {
+        console.log('Hello world')
+    }
+
+
     render() {
         const { name, email, phone } = this.props.contact;
         return (
             <div className="card card-body mb-3">
                 <h4>
-                    {name} <i className="fas fa-sort-down" />
+                    {name} <i 
+                    onClick={this.onShowClick}
+                    className="fas fa-sort-down" />
                 </h4>
                 <ul className="list-group">
                 <li className="list-group-item">Email: {email}</li>
