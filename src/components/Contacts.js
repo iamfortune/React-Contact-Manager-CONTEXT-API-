6778d8b27f2dcb0fa42280f35bda4beeb5075a9a
iamfortune <ikechifortune@gmail.com>
2020-03-14 15:54:55 +0100

    fixed bug that arose from not positioning closing parenthesis } well

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -2,9 +2,8 @@ import React, { Component } from 'react'
 import Contact from './Contact'
 
 class Contacts extends Component {
-    constructor() {
-        super();
-        this.state = {
+    
+    state = {
           contacts: [
             {
               id: 1,
@@ -26,22 +25,26 @@ class Contacts extends Component {
             }
           ]
         };
-    }
+
+
     render() {
         const { contacts } = this.state;
+
         return (
           
             <div>
                 {contacts.map(contact => (
                 <Contact 
+                key={contact.id}
                  name={contact.name}
                  email={contact.email}
                  phone={contact.phone}
                  />
                 ))}
             </div>
-        )
+        );
     }
+  
 }
 
 export default Contacts;
