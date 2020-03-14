d62a4c5110efed21003d35774ba150218f5654c2
iamfortune <ikechifortune@gmail.com>
2020-03-14 13:03:42 +0100

    added new contacts to the contact component

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -4,14 +4,27 @@ class Contacts extends Component {
     constructor() {
         super();
         this.state = {
-            contacts: [
-                {
-                    name: 'John Doe',
-                    email: 'jide@gmail.com',
-                    phone: '08109526245'
-                }
-            ]
-        }
+          contacts: [
+            {
+              id: 1,
+              name: "John Doe",
+              email: "jide@gmail.com",
+              phone: "08109526245"
+            },
+            {
+              id: 2,
+              name: "Francy Ana",
+              email: "francy@gmail.com",
+              phone: "08109526567"
+            },
+            {
+              id: 3,
+              name: "Chioma B",
+              email: "chiomab@gmail.com",
+              phone: "08109526123"
+            }
+          ]
+        };
     }
     render() {
         return (
