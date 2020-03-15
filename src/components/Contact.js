ced520e6f867665fcac4d836c7dea08c050a3014
iamfortune <ikechifortune@gmail.com>
2020-03-15 07:20:26 +0100

    added click event function

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,24 +4,31 @@ import PropTypes from 'prop-types';
 
 class Contact extends Component {
     state = {
-        showContactInfo: true
+        showContactInfo: false
     };
 
   
     render() {
         const { name, email, phone } = this.props.contact;
+        const { showContactInfo } = this.state;
+
         return (
             <div className="card card-body mb-3">
                 <h4>
-                    {name} <i 
-                    onClick={() => this.setState({
-                        showContactInfo: !this.state.showContactInfo })}
+                    {name}{' '} 
+                    <i
+                    onClick={() => 
+                        this.setState({ showContactInfo: 
+                        !this.state.showContactInfo })
+                    }
                     className="fas fa-sort-down" />
                 </h4>
-                <ul className="list-group">
+                {showContactInfo ? (
+                     <ul className="list-group">
                 <li className="list-group-item">Email: {email}</li>
                 <li className="list-group-item">Phone: {phone}</li>
                 </ul>
+                ) : null}
             </div>
         );
     }
