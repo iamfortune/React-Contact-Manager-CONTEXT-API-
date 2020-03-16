11fb315bf65b6bf02f15b9840ae94059fce7c405
iamfortune <ikechifortune@gmail.com>
2020-03-16 12:38:04 +0100

    added new state and made it easier fr the icon to delete

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -29,13 +29,13 @@ class Contacts extends Component {
           deleteContact = (id) => {
               const { contacts } = this.state;
 
-              const newContact = contacts.filter(contact => 
+              const newContacts = contacts.filter(contact => 
                 contact.id !== id);
 
                 this.setState({
-                  contacts: newContact
+                  contacts: newContacts
                 });
-          }
+          };
 
     render() {
         const { contacts } = this.state;
