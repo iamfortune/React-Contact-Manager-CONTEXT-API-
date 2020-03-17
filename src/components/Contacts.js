0d77d033427c2c1f80daa38d055b5130df8f20b1
iamfortune <ikechifortune@gmail.com>
2020-03-17 15:33:58 +0100

    modified files

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -3,34 +3,21 @@ import Contact from './Contact';
 import { Consumer } from '../Context';
 
 class Contacts extends Component {
-  
-
-          deleteContact = (id) => {
-
-              const { contacts } = this.state;
-
-              const newContacts = contacts.filter(contact => 
-                contact.id !== id);
-
-                this.setState({
-                  contacts: newContacts
-                });
-          };
-
     render() {
       return (
         <Consumer >
           {value => {
 
             const { contacts } = value;
-            
+
             return (
             
             <React.Fragment>
                 {contacts.map(contact => (
                 <Contact 
                    key={contact.id}
-                   contact={contact} deleteClickHandler={this.deleteContact.bind(this, contact.id)} />
+                   contact={contact}  
+                   />
                 ))}
             </React.Fragment>
         );
