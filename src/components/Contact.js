b130d4bf231f30035f8046a552fd2e1b47d5567c
iamfortune <ikechifortune@gmail.com>
2020-03-18 12:11:03 +0100

    enclosed the delete button div in a consumer tag so we can be able to make sure of the state in consumer which is inside of context

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Consumer } from '../Context';
 
 
 class Contact extends Component {
@@ -12,30 +13,46 @@ class Contact extends Component {
     }
   
     render() {
-        const { name, email, phone } = this.props.contact;
+        const { name, email, phone } = 
+        this.props.contact;
         const { showContactInfo } = this.state;
 
         return (
-            <div className="card card-body mb-3">
-                <h4>
-                    {name}{' '} 
-                    <i
-                    onClick={() => 
-                        this.setState({ showContactInfo: 
-                        !this.state.showContactInfo })
-                    }
-                    className="fas fa-sort-down" style={{ cursor: 'pointer'}}/>
-                    <i className="fas fa-times" style={{ cursor: 'pointer', float: 'right', color: 'red' }} 
-                    onClick={this.onDeleteClick}
-                    />
-                </h4>
-                {showContactInfo ? (
-                     <ul className="list-group">
-                <li className="list-group-item">Email: {email}</li>
-                <li className="list-group-item">Phone: {phone}</li>
-                </ul>
-                ) : null}
-            </div>
+            <Consumer>
+                {value => {
+                    return (
+                      <div className="card card-body mb-3">
+                        <h4>
+                          {name}{" "}
+                          <i
+                            onClick={() =>
+                              this.setState({
+                                showContactInfo: !this.state.showContactInfo
+                              })
+                            }
+                            className="fas fa-sort-down"
+                            style={{ cursor: "pointer" }}
+                          />
+                          <i
+                            className="fas fa-times"
+                            style={{
+                              cursor: "pointer",
+                              float: "right",
+                              color: "red"
+                            }}
+                            onClick={this.onDeleteClick}
+                          />
+                        </h4>
+                        {showContactInfo ? (
+                          <ul className="list-group">
+                            <li className="list-group-item">Email: {email}</li>
+                            <li className="list-group-item">Phone: {phone}</li>
+                          </ul>
+                        ) : null}
+                      </div>
+                    );
+                }}
+            </Consumer>
         );
     }
 }
