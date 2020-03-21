9af5821b89853dd9e7016daea5c1d33cdee2bd13
iamfortune <ikechifortune@gmail.com>
2020-03-21 10:15:27 +0100

    added consumer to contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -17,26 +17,43 @@ class Contact extends Component {
         const { showContactInfo } = this.state;
 
         return (
-            <div className="card card-body mb-3">
-                <h4>
-                    {name}{' '} 
-                    <i
-                    onClick={() => 
-                        this.setState({ showContactInfo: 
-                        !this.state.showContactInfo })
-                    }
-                    className="fas fa-sort-down" style={{ cursor: 'pointer'}}/>
-                    <i className="fas fa-times" style={{ cursor: 'pointer', float: 'right', color: 'red' }} 
-                    onClick={this.onDeleteClick}
-                    />
-                </h4>
-                {showContactInfo ? (
-                     <ul className="list-group">
-                <li className="list-group-item">Email: {email}</li>
-                <li className="list-group-item">Phone: {phone}</li>
-                </ul>
-                ) : null}
-            </div>
+            <Consumer>
+                {value => {
+                    const { dispatch } =  value;
+                    return (
+                      <div className="card card-body mb-3">
+                        <h4>
+                          {name}{" "}
+                          <i
+                            onClick={() =>
+                              this.setState({
+                                showContactInfo: !this.state.showContactInfo
+                              })
+                            }
+                            className="fas fa-sort-down"
+                            style={{ cursor: "pointer" }}
+                          />
+                          <i
+                            className="fas fa-times"
+                            style={{
+                              cursor: "pointer",
+                              float: "right",
+                              color: "red"
+                            }}
+                            onClick={this.onDeleteClick.bind
+                                (this, id, dispatch)}
+                          />
+                        </h4>
+                        {showContactInfo ? (
+                          <ul className="list-group">
+                            <li className="list-group-item">Email: {email}</li>
+                            <li className="list-group-item">Phone: {phone}</li>
+                          </ul>
+                        ) : null}
+                      </div>
+                    );
+                }}
+            </Consumer>
         );
     }
 }
