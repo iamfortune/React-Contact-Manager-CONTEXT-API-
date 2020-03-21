fedb8277d3bb1e9d26dcefbc1c57345803c69e9a
iamfortune <ikechifortune@gmail.com>
2020-03-21 20:03:58 +0100

    finally worked out bug that prevented delete icpon from working, this was done because i spelt filter as fiter

--- src/Context.js
+++ src/Context.js
@@ -1,54 +1,53 @@
-import React, {Component} from 'react'
+import React, { Component } from "react";
 
 const Context = React.createContext();
 
 const reducer = (state, action) => {
-  switch(action.type) {
-    case 'DELETE_CONTACT':
+  switch (action.type) {
+    case "DELETE_CONTACT":
       return {
         ...state,
-        contacts: state.contacts.fiter(contact =>
-          contact.id !== action.payload)
-      }
-      default: 
+        contacts: state.contacts.filter(
+          contact => contact.id !== action.payload
+          )
+      };
+    default:
       return state;
+  }
+};
+
+export class Provider extends Component {
+  state = {
+    contacts: [
+      {
+        id: 1,
+        name: "John Doe",
+        email: "jide@gmail.com",
+        phone: "08109526245"
+      },
+      {
+        id: 2,
+        name: "Francy Ana",
+        email: "francy@gmail.com",
+        phone: "08109526567"
+      },
+      {
+        id: 3,
+        name: "Chioma B",
+        email: "chiomab@gmail.com",
+        phone: "08109526123"
+      }
+    ],
+    dispatch: action => this.setState(state => reducer(state, action))
+  };
+
+  render() {
+    return (
+      <Context.Provider value={this.state}>
+        {this.props.children}
+      </Context.Provider>
+    );
   }
 }
 
-
-export class Provider extends Component {
-         state = {
-           contacts: [
-             {
-               id: 1,
-               name: "John Doe",
-               email: "jide@gmail.com",
-               phone: "08109526245"
-             },
-             {
-               id: 2,
-               name: "Francy Ana",
-               email: "francy@gmail.com",
-               phone: "08109526567"
-             },
-             {
-               id: 3,
-               name: "Chioma B",
-               email: "chiomab@gmail.com",
-               phone: "08109526123"
-             }
-           ],
-           dispatch: action => 
-             this.setState(state => reducer(state, action))
-         };
-
-         render() {
-             return (
-                 <Context.Provider value={this.state}>
-                     {this.props.children}
-                 </Context.Provider>
-             )
-         }
-       }
-
-       export const Consumer = Context.Consumer; 
+export const Consumer = Context.Consumer;
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Consumer } from '../context';
+import { Consumer } from '../Context';
 
 
 class Contact extends Component {
