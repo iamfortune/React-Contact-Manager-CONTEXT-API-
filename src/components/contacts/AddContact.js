2c376f7d6692b086e26c8e4939dd2ecc13f0388c
iamfortune <ikechifortune@gmail.com>
2020-03-22 00:01:56 +0100

    added class to addcontact component

--- src/components/contacts/AddContact.js
+++ src/components/contacts/AddContact.js
@@ -1,2 +1,13 @@
 import React, { Component } from 'react';
 
+class AddContact extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default AddContact;
