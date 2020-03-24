bd1ee70e54b01903b92e723ec0f3af869a8efc19
iamfortune <ikechifortune@gmail.com>
2020-03-24 19:31:40 +0100

    added a form attribute for input

--- src/components/contacts/AddContact.js
+++ src/components/contacts/AddContact.js
@@ -14,7 +14,13 @@ class AddContact extends Component {
                <div className="card-header">Add Contact</div>
                <div className="card-body">
                    <form>
-                       
+                       <div className="form-group">
+                           <label htmlFor="name">Name</label>
+                           <input
+                            type="text" className="form-control form-control-lg"
+                            
+                            />
+                       </div>
                    </form>
                </div>
             </div>
