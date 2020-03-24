7b9d6fb6920cfd6fde9074ea5c720c41b012e45c
iamfortune <ikechifortune@gmail.com>
2020-03-24 19:22:59 +0100

    added contact to app

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ class App extends Component {
       <div className="App">
         <Header />
         <div className="container">
+          <AddContact />
         <Contacts />
         </div>
       </div>
