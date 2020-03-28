28ed8ab33c52f7a1c40a743353fb32fcf22ce049
iamfortune <ikechifortune@gmail.com>
2020-03-28 19:29:57 +0100

    added a constructor, super and a ref to the addContact component

--- src/components/contacts/AddContact.js
+++ src/components/contacts/AddContact.js
@@ -1,6 +1,13 @@
 import React, { Component } from "react";
 
 class AddContact extends Component {
+    constructor(props) {
+        super(props);
+
+        this.nameInput = React.createRef
+    }
+
+
   // submit method for form submit button
   onSubmit = e => {
     e.preventDefault();
