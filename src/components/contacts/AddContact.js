062c4737ae8bb07cbec23fce7ab7f5a3ed9b5532
iamfortune <ikechifortune@gmail.com>
2020-03-29 16:02:00 +0100

    added dispatch to addcontact

--- src/components/contacts/AddContact.js
+++ src/components/contacts/AddContact.js
@@ -27,59 +27,61 @@ class AddContact extends Component {
             {value => {
               const { dispatch } = value;
               return (
-                     <div className="card mb-3">
-            <div className="card-header">Add Contact</div>
-            <div className="card-body">
-                {/* form submit handler */}
-              <form onSubmit={this.onSubmit}>
-                {/* text input for name */}
-                <div className="form-group">
-                  <label htmlFor="name">Name</label>
-                  <input
-                    type="text"
-                    name="name"
-                    className="form-control form-control-lg"
-                    placeholder="Enter Name..."
-                    value={name}
-                    onChange={this.onChange}
-                  />
+                <div className="card mb-3">
+                  <div className="card-header">Add Contact</div>
+                  <div className="card-body">
+                    {/* form submit handler */}
+                    <form onSubmit={this.onSubmit}>
+                      {/* text input for name */}
+                      <div className="form-group">
+                        <label htmlFor="name">Name</label>
+                        <input
+                          type="text"
+                          name="name"
+                          className="form-control form-control-lg"
+                          placeholder="Enter Name..."
+                          value={name}
+                          onChange={this.onChange}
+                        />
+                      </div>
+                      {/* Text input for email */}
+                      <div className="form-group">
+                        <label htmlFor="email">Email</label>
+                        <input
+                          type="email"
+                          name="email"
+                          className="form-control form-control-lg"
+                          placeholder="you@example.com"
+                          value={email}
+                          onChange={this.onChange}
+                        />
+                      </div>
+                      {/* text input for phone */}
+                      <div className="form-group">
+                        <label htmlFor="phone">phone</label>
+                        <input
+                          type="text"
+                          name="phone"
+                          className="form-control form-control-lg"
+                          placeholder="Enter Phone Number..."
+                          value={phone}
+                          onChange={this.onChange}
+                        />
+                      </div>
+                      {/* Adding a submit button for the form */}
+                      <input
+                        type="submit"
+                        value="Add Contact"
+                        className="btn btn-light btn-block"
+                      />
+                    </form>
+                  </div>
                 </div>
-                {/* Text input for email */}
-                <div className="form-group">
-                  <label htmlFor="email">Email</label>
-                  <input
-                    type="email"
-                    name="email"
-                    className="form-control form-control-lg"
-                    placeholder="you@example.com"
-                    value={email}
-                    onChange={this.onChange}
-                  />
-                </div>
-                {/* text input for phone */}
-                <div className="form-group">
-                  <label htmlFor="phone">phone</label>
-                  <input
-                    type="text"
-                    name="phone"
-                    className="form-control form-control-lg"
-                    placeholder="Enter Phone Number..."
-                    value={phone}
-                    onChange={this.onChange}
-                  />
-                </div>
-                {/* Adding a submit button for the form */}
-                <input type="submit" value="Add Contact"
-                className="btn btn-light btn-block"
-                />
-              </form>
-            </div>
-              )
+              );
             }}
           </Consumer>
         )
-          </div>
-        );
+
     }
 }
 
