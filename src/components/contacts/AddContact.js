309e0e625ba4e8100a66ec1aaf6edd16e04e0e85
iamfortune <ikechifortune@gmail.com>
2020-04-01 18:48:09 +0100

    added new parameters to make input possible

--- src/components/contacts/AddContact.js
+++ src/components/contacts/AddContact.js
@@ -5,21 +5,63 @@ class AddContact extends Component {
         name: '',
         email: '',
         phone: ''
-    }
+    };
+
+    
+
+    onChange = e => this.setState({ [e.target.name]: e.target.value });
+
+
     render() {
+        const { name, email, phone } = this.state;
         return (
-            <div className="carb mb-3">
-                <div className="card-header">Add Contact</div>
-                <div className="card-body">
-                    <form>
-                        <div className="form-group">
-                            <label htmlFor="name">Name</label>
-                            <input type="text" className="form-control
-                             form-control-lg"/>
-                        </div>
-                    </form>
+          <div className="carb mb-3">
+            <div className="card-header">Add Contact</div>
+            <div className="card-body">
+              <form onSubmit={this.onSubmit}>
+                <div className="form-group">
+                  <label htmlFor="name">Name</label>
+                  <input
+                    type="text"
+                    name="name"
+                    className="form-control form-control-lg"
+                    placeholder="Enter Name..."
+                    value={name}
+                    onChange={this.onChange}
+                  />
                 </div>
+                {/* form group for email */}
+                <div className="form-group">
+                  <label htmlFor="email">Email</label>
+                  <input
+                    type="email"
+                    name="email"
+                    className="form-control form-control-lg"
+                    placeholder="you@example.com"
+                    value={email}
+                    onChange={this.onChange}
+                  />
+                </div>
+                {/* form group for phone number */}
+                <div className="form-group">
+                  <label htmlFor="phone">Phonee</label>
+                  <input
+                    type="text"
+                    name="phone"
+                    className="form-control form-control-lg"
+                    placeholder="Enter Phone..."
+                    value={phone}
+                    onChange={this.onChange}
+                  />
+                </div>
+                <input
+                  type="submit"
+                  value="Add Contact"
+                  className="btn btn-light n  btn-block"
+                />
+              </form>
             </div>
+          </div>
         );
     }
 }
