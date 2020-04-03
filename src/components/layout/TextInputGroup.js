75e4e8fd002624c60bd9f02762996b2343abd0e1
iamfortune <ikechifortune@gmail.com>
2020-04-03 12:37:33 +0100

    defined propTypes in TextInputGroup

--- src/components/layout/TextInputGroup.js
+++ src/components/layout/TextInputGroup.js
@@ -1,8 +1,27 @@
 import React from 'react'
 import PropTypes from 'prop-types';
 
-const  TextInputGroup = () => {
-   return <div />;
+const  TextInputGroup = ({
+    label,
+    name,
+    value,
+    placeholder,
+    type,
+    onChange
+}) => {
+   return (
+     <div className="form-group">
+       <label htmlFor="name">Name</label>
+       <input
+         type="text"
+         name="name"
+         className="form-control form-control-lg"
+         placeholder="Enter Name..."
+         value={name}
+         onChange={this.onChange}
+       />
+     </div>
+   );
 };
 
 export default TextInputGroup;
