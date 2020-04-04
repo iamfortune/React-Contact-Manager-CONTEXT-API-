3eefbb90d8cd4d7b9baf1d53eaf42e2df202c1a4
iamfortune <ikechifortune@gmail.com>
2020-04-04 12:31:26 +0100

    imported header and about, and added routes to the ./pages/About.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Contacts from './components/contacts/Contacts';
-import Header from "./components/layout/Header";
 import AddContact from "./components/contacts/AddContact";
+import Header from "./components/layout/Header";
+import About from "./components/pages/About";
 
 import { Provider } from './Context';
 
@@ -18,11 +19,10 @@ class App extends Component {
           <div className="App">
             <Header />
             <div className="container">
-             <Switch>
-               <Route exact path="/" component=
-               {Contacts} />
-              
-             </Switch>
+              <Switch>
+                <Route exact path="/" component={Contacts} />
+                <Route exact path="/about" component={About} />
+              </Switch>
             </div>
           </div>
         </Router>
