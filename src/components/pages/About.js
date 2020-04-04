74cdb9869bb42ef458697e7ce63fe62d58279962
iamfortune <ikechifortune@gmail.com>
2020-04-04 12:22:42 +0100

    added texts and inputs to the about page

---
+++ src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function About() {
+    return (
+        <div>
+            <h1 className="display-4">About Kay's Contact Manager </h1>
+            <p className="lead">A simple app to manage contatcts for businesses and organizations</p>
+            <p className="text-secondary">Version 1.0.0</p>
+        </div>
+    )
+}
+
+export default About;
