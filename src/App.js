9a8792a72608001089744031274bc2a68c5dced4
iamfortune <ikechifortune@gmail.com>
2020-04-04 12:13:15 +0100

    added a router to the component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Contacts from './components/contacts/Contacts';
 import Header from "./components/layout/Header";
 import AddContact from "./components/contacts/AddContact";
@@ -13,13 +14,18 @@ class App extends Component {
   render() {
     return (
       <Provider>
-      <div className="App">
-        <Header />
-        <div className="container">
-          <AddContact/>
-        <Contacts />
-        </div>
-      </div>
+        <Router>
+          <div className="App">
+            <Header />
+            <div className="container">
+             <Switch>
+               <Route exact path="/" component=
+               {Contacts} />
+              
+             </Switch>
+            </div>
+          </div>
+        </Router>
       </Provider>
     );
   }
