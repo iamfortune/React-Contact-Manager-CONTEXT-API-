01da9273b019a8915ab4238370cfa4fb9bb8caa1
iamfortune <ikechifortune@gmail.com>
2020-04-04 16:23:47 +0100

    added bootstrap class for changing contact color to red in the about page

--- src/components/contacts/Contacts.js
+++ src/components/contacts/Contacts.js
@@ -10,6 +10,9 @@ class Contacts extends Component {
             const { contacts } = value;
             return (
             <React.Fragment>
+              <h1 className="display-4 mb-2">
+                <span className="text-danger">Contact </span> List
+              </h1>
                 {contacts.map(contact => (
                 <Contact key={contact.id} contact= 
                    {contact} />
