13a640e5d81c88acff26b6d7ec9d1c95cffee6d7
iamfortune <ikechifortune@gmail.com>
2020-04-05 22:28:32 +0100

    added more links for add contact and about pages

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -4,22 +4,29 @@ import { Link } from 'react-router-dom'
 
 const Header = (props) => {
     const { branding } = props;
-    return(
-        <nav className="navbar navbar-expand-sm navbar-dark bg-danger mb-3 py-0">
-            <div className="container">
-                <a href="/" className="navbar-brand">
-                    {branding}</a>
-                    <div>
-                        <ul className="navbar-nav mr-auto">
-                            <li className="nav-item">
-                                <Link to="/" className="nav-link">
-                                    Home
-                                </Link>
-                            </li>
-                        </ul>
-                    </div>
-            </div>
-        </nav>
+    return (
+      <nav className="navbar navbar-expand-sm navbar-dark bg-danger mb-3 py-0">
+        <div className="container">
+          <a href="/" className="navbar-brand">
+            {branding}
+          </a>
+          <div>
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item">
+                <Link to="/" className="nav-link">
+                  Home
+                </Link>
+                <Link to="/contact/add" className="nav-link">
+                  Add
+                </Link>
+                <Link to="/about" className="nav-link">
+                  About
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     );
 }
 
