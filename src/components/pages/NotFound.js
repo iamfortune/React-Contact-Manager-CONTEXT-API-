5a1d027be9c59f58fa8806404b9415bf3a4466d3
iamfortune <ikechifortune@gmail.com>
2020-04-06 19:43:20 +0100

    added p text

--- src/components/pages/NotFound.js
+++ src/components/pages/NotFound.js
@@ -4,6 +4,7 @@ function NotFound() {
     return (
         <div>
             <h1 className="display-4">404 Page Not Found</h1>
+            <p className="lead">Sorry, page does not exist! </p>
         </div>
     );
 }
