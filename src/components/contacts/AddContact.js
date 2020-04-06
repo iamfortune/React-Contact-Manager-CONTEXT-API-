c96b271331a000d80c6fd8902530f53b5e5a264e
iamfortune <ikechifortune@gmail.com>
2020-04-06 19:35:52 +0100

    added a push back function to redirect between the addcontact and index page

--- src/components/contacts/AddContact.js
+++ src/components/contacts/AddContact.js
@@ -50,7 +50,10 @@ class AddContact extends Component {
             phone: '',
             errors: {}
         });
-    }
+
+        this.props.history.push('/');
+
+    };
 
     onChange = e => this.setState({ [e.target.name]: e.target.value });
 
