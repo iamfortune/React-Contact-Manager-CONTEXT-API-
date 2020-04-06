ca9f50395a3a9c2632bc8f005eadc296474c0815
iamfortune <ikechifortune@gmail.com>
2020-04-06 19:41:50 +0100

    added text to notfound component

--- src/components/pages/NotFound.js
+++ src/components/pages/NotFound.js
@@ -3,9 +3,9 @@ import React from 'react'
 function NotFound() {
     return (
         <div>
-            
+            <h1 className="display-4">404 Page Not Found</h1>
         </div>
-    )
+    );
 }
 
 export default NotFound;
