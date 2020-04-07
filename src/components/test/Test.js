1fa166aa1e11f69bcca2569925b61e5cce6f985e
iamfortune <ikechifortune@gmail.com>
2020-04-07 20:39:02 +0100

    added an empty state and also called an epi using fetch and the jsonplaceholder

--- src/components/test/Test.js
+++ src/components/test/Test.js
@@ -2,48 +2,52 @@ import React, { Component } from 'react'
 
 class Test extends Component {
     state = {
-        test: 'test'
+        title: '',
+        completed: ''
+    };
+
+    componentDidMount() {
+        fetch("https://jsonplaceholder.typicode.com/todos/1")
+          .then(response => response.json())
+          .then(data => this.setState());
     }
 
 
+//   componentDidMount() {
+//     console.log("componentDidmount");
+//     //usually good for making AJAX calls and API requests
+//   }
 
-  componentDidMount() {
-    console.log("componentDidmount");
-    //usually good for making AJAX calls and API requests
-  }
+//   UNSAFE_componentWillMount() {
+//       console.log('componentWillMount');
+//   }
 
-  UNSAFE_componentWillMount() {
-      console.log('componentWillMount');
-  }
+//   UNSAFE_componentDidUpdate() {
+//       console.log('componentDidUpdate');
+//   }
 
-  UNSAFE_componentDidUpdate() {
-      console.log('componentDidUpdate');
-  }
+//   UNSAFE_componentWillUpdate() {
+//       console.log('componentWillUpdate');
+//   }
 
-  UNSAFE_componentWillUpdate() {
-      console.log('componentWillUpdate');
-  }
+//   UNSAFE_componentWillReceiveProps(nextProps, nextState) {
+//       console.log('componentWillReceiveProps');
+//       //usually used in redux, for when you have real states in redux
+//   }
 
-  UNSAFE_componentWillReceiveProps(nextProps, nextState) {
-      console.log('componentWillReceiveProps');
-      //usually used in redux, for when you have real states in redux
-  }
-
-  static getDerivedStateFromProps(nextProps, prevstate) {
-      return {
-          test: 'fortune ikechi'
-      }
-       //this is a new version of componentWillReceiveProps
-  }
+//   static getDerivedStateFromProps(nextProps, prevstate) {
+//       return {
+//           test: 'fortune ikechi'
+//       }
+//        //this is a new version of componentWillReceiveProps
+//   }
      
-  getSnapshotBeforeUpdate(prevProps, prevState) {
-      console.log('componentWillReceiveProps...');
-      //not always used in applications, still new
-  }
+//   getSnapshotBeforeUpdate(prevProps, prevState) {
+//       console.log('componentWillReceiveProps...');
+//       //not always used in applications, still new
+//   }
       
   
-
-
   render() {
     return (
       <div>
