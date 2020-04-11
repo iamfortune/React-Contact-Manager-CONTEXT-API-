2283a9a671a6faecb1c2cd77ee572d7ccfc028d4
iamfortune <ikechifortune@gmail.com>
2020-04-11 22:53:37 +0100

    Updates

---
+++ precache-manifest.a8b8ff1120c646770d6ef6a60914ffb5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a438833990dd621eb4673258e2e90d7",
+    "url": "/kay-contact-manager/index.html"
+  },
+  {
+    "revision": "a3a09fa1feb2c9e057d0",
+    "url": "/kay-contact-manager/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "3f6dafc64fa8166c2f96",
+    "url": "/kay-contact-manager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "a3a09fa1feb2c9e057d0",
+    "url": "/kay-contact-manager/static/js/2.c690ea2b.chunk.js"
+  },
+  {
+    "revision": "56729d980da09961afe8d497a21eec12",
+    "url": "/kay-contact-manager/static/js/2.c690ea2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f6dafc64fa8166c2f96",
+    "url": "/kay-contact-manager/static/js/main.fd51115a.chunk.js"
+  },
+  {
+    "revision": "665525211118af306795",
+    "url": "/kay-contact-manager/static/js/runtime-main.67c88432.js"
+  }
+]);
