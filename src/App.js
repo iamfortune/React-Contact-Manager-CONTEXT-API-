a0ba48c712971f74ce9d73d52903575877d3f9b6
iamfortune <ikechifortune@gmail.com>
2020-04-11 22:44:30 +0100

    changed broswerRouter to HashRouter to make site worrk on github pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import Contacts from './components/contacts/Contacts';
 import AddContact from "./components/contacts/AddContact";
 import EditContact from './components/contacts/EditContact';
