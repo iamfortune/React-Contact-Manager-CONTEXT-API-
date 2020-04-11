b57011b24f9e86a6a3da1240e6071005b7350435
iamfortune <ikechifortune@gmail.com>
2020-04-11 16:23:32 +0100

    added dispatch and an add contact action to the context API

--- src/components/contacts/EditContact.js
+++ src/components/contacts/EditContact.js
@@ -11,6 +11,21 @@ class EditContact extends Component {
     errors: {},
   };
 
+  async componentDidMount() {
+      const { id } = this.props.match.params;
+      const res = await axios.get(`https://jsonplaceholder.typicode.com/users/${id}`);
+
+      const contact = res.data;
+
+      this.setState({
+        name: contact.name,
+        email: contact.email,
+        phone: contact.phone
+      })
+  }
+
+
+
   onSubmit = async (dispatch, e) => {
     e.preventDefault();
 
@@ -31,6 +46,19 @@ class EditContact extends Component {
       this.setState({ errors: { phone: "phone is required" } });
       return;
     }
+
+    const updContact = {
+        name,
+        email,
+        phone 
+    }
+
+    const { id } = this.props.match.params;
+
+    const res = await axios.put(`https://jsonplaceholder.typicode.com/users/${id}`, updContact);
+
+    dispatch({type: 'UPDATE_CONTACT', payload: 
+    res.data});
 
    
     //clear state
