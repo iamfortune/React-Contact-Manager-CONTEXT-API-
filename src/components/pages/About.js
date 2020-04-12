74b5f33f4fed5bdea5d7f1bc142bb8332d301282
Fortune Ikechi <ikechifortune@gmail.com>
2020-04-12 07:33:56 +0100

    Update About.js

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -4,7 +4,7 @@ function About() {
     return (
         <div>
             <h1 className="display-4">About Kay's Contact Manager </h1>
-            <p className="lead">A simple app to manage contatcts for businesses and organizations</p>
+            <p className="lead">A simple app to manage contacts for businesses and organizations</p>
             <p className="text-secondary">Version 1.0.0</p>
         </div>
     )
